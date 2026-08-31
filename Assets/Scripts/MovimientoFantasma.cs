98ac83ea0e2ca30c69f8f6b7ddc6f6926d70d951
SanchoPanz4 <csanchezva@ing.ucsc.cl>
2026-08-31 16:07:05 -0400

    cambios en fantasma y paredes

--- Assets/Scripts/MovimientoFantasma.cs
+++ Assets/Scripts/MovimientoFantasma.cs
@@ -5,6 +5,7 @@ public class Movimiento : MonoBehaviour
     public float velFantasma = 5f;
     public float radioDeteccion = 3.0f;
     public LayerMask capaParedes;
+    public Vector3 vectorMov = new Vector3(0.0f, 0.0f, 1.0f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +14,28 @@ public class Movimiento : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Paso por: " + gameObject.name);
+        Debug.Log("Choco con: " + collision.gameObject.name);
+        randomVector();
+    }
+    void randomVector()
+    {
+        if(vectorMov.z == 1)
+        {
+            vectorMov.x = 1;
+            vectorMov.z = 0;
+        }
+        else if(vectorMov.x == 1)
+        {
+            vectorMov.x = 0;
+            vectorMov.z = -1;
+        }
+        /*
+        float x = (float)Random.Range(0,1);
+        float y = 0.0f;
+        float z = (float)Random.Range(0,1);
+        Vector3 vector = new Vector3(x,y,z);
+        return vector;
+        */
     }
 /*    void DetectarObjetos()
     {   
@@ -28,6 +50,6 @@ public class Movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * velFantasma * Time.deltaTime);
+        transform.Translate(vectorMov * velFantasma * Time.deltaTime);
     }
 }
